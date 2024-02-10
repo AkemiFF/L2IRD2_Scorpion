1a8f7ca69bd6f19a9e0e7dafb4a8aa2298a9fa9d
Brad Akemi <mirado.akemi@gmail.com>
2024-02-10 22:07:23 +0300

    Modification du model

--- GestionPlaintes/accounts/views.py
+++ GestionPlaintes/accounts/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 
 
 def index(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
-        print(email)
-    return render(request, 'acceuil.html')
+        User = get_user_model()
+        try:
+            existing_user = User.objects.get(email=email)
+            return render(request, 'existing_user_error.html')
+        except User.DoesNotExist:
+            user = User.objects.create_user(email=email, password=password)
+            return render(request, 'user_created_successfully.html')
+
+    return render(request, 'index.html')
