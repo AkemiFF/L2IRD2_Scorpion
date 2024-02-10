75d4e6a43492614aa190016f8e1e6b214777b25a
Brad Akemi <mirado.akemi@gmail.com>
2024-02-10 21:39:54 +0300

    ajout d'un page html acceuil

--- GestionPlaintes/GestionPlaintes/urls.py
+++ GestionPlaintes/GestionPlaintes/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('acceuil/', include('UserPart.urls')),
     path('population/', include('UserPart.urls')),
 ]
