850f690092000c6239c02e659a30a1f63c9ff053
Brad Akemi <mirado.akemi@gmail.com>
2024-02-10 22:13:46 +0300

    views logic changed

--- GestionPlaintes/accounts/views.py
+++ GestionPlaintes/accounts/views.py
@@ -5,14 +5,23 @@ from django.db import IntegrityError
 
 def index(request):
     if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        User = get_user_model()
-        try:
-            existing_user = User.objects.get(email=email)
-            return render(request, 'existing_user_error.html')
-        except User.DoesNotExist:
-            user = User.objects.create_user(email=email, password=password)
-            return render(request, 'user_created_successfully.html')
+        if 'email' in request.POST and 'password' in request.POST:
+            email = request.POST['email']
+            password = request.POST['password']
+
+        elif 'email-inscrip' in request.POST and 'password-inscrip' in request.POST:
+            email = request.POST['email-inscrip']
+            password = request.POST['password-inscrip']
+            first_name = request.POST.get('surname', '')
+            last_name = request.POST.get('name', '')
+
+            User = get_user_model()
+            try:
+                existing_user = User.objects.get(email=email)
+                return render(request, 'existing_user_error.html')
+            except User.DoesNotExist:
+                user = User.objects.create_user(email=email, password=password, first_name=first_name,
+                                                last_name=last_name)
+                return render(request, 'acceuil.html')
 
     return render(request, 'index.html')
