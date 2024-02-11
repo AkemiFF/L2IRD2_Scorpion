a4f66eb2b4a4bc05998912e74b5802d8a27fa98a
Brad Akemi <mirado.akemi@gmail.com>
2024-02-11 03:55:20 +0300

    changement vers message

--- GestionPlaintes/UserPart/urls.py
+++ GestionPlaintes/UserPart/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('contact/', views.contact, name="contact"),
     path('probleme/', views.probleme, name="probleme"),
-    path('service/', views.service, name="service"),
+    path('service/', views.messagerie, name="messagerie"),
     path('suggestion/', views.suggestion, name="suggestion"),
 ]
