b4e7b359105ded1d6893793d2fdf344bf66d6991
Brad Akemi <mirado.akemi@gmail.com>
2024-02-11 12:43:16 +0300

    reglage affichage image

--- GestionPlaintes/GestionPlaintes/urls.py
+++ GestionPlaintes/GestionPlaintes/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from GestionPlaintes import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('acceuil/', include('UserPart.urls')),
     path('responsible/', include('BackOffice.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
